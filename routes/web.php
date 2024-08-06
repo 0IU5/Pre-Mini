031b4e95db1c38898835b3e2cb2f3d8856b48123
0IU5 <riopinayungan@github.com>
2024-08-06 07:58:26 +0700

    membuat_profile

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,9 @@
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\GuruController;
 use App\Http\Controllers\PaketController;
-use App\Http\Controllers\AuthController;
 use App\Http\Controllers\JadwalController;
 use App\Http\Controllers\PaymentController;
 use App\Http\Controllers\MapelController;
@@ -15,57 +15,48 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-}); 
+});
 
 Route::get('/landing-page', function () {
-    return view('landing-page');    
-}); 
+    return view('landing-page');
+});
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->name('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-// ->middleware(['auth', 'verified'])
+// Rute untuk registrasi
+Route::get('register', [AuthController::class, 'register'])
+    ->middleware('guest')->name('register');
+Route::post('register', [AuthController::class, 'registerSave'])
+    ->middleware('guest')->name('register.save');
 
-Route::get('/profile', function () {
-    return view('profile');    
-}); 
-
-// // Rute untuk registrasi
-// Route::get('register', [AuthController::class, 'register'])
-//     ->middleware('guest')->name('register');
-// Route::post('register', [AuthController::class, 'registerSave'])
-//     ->middleware('guest')->name('register.save');
-
-// // Rute untuk login
-// Route::get('login', [AuthController::class, 'login'])
-//     ->middleware('guest')->name('login');
-// Route::post('login', [AuthController::class, 'loginAction'])
-//     ->middleware('guest')->name('login.action');
+// Rute untuk login
+Route::get('login', [AuthController::class, 'login'])
+    ->middleware('guest')->name('login');
+Route::post('login', [AuthController::class, 'loginAction'])
+    ->middleware('guest')->name('login.action');
 
 // Rute untuk logout
 Route::get('logout', [AuthController::class, 'logout'])
     ->middleware('auth')->name('logout');
 
 Route::middleware('auth')->group(function () {
-    Route::get('profile', [AuthController::class, 'profile'])->name('profile');
-
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::put('/profile', [ProfileController::class, 'update'])->name('profile.update'); 
+    Route::get('/profile', [ProfileController::class, 'index'])->name('profile.index');
+    Route::get('/profile/edit', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::put('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    // Route::resource('register', RegisteredUserController::class);
-    // Route::resource('login', AuthenticatedSessionController::class);
-
-    Route::get('/search', [SearchController::class, 'index'])->name('search');
-});
-
-    Route::resource('paket', PaketController::class);
-    Route::resource('guru', GuruController::class);
+    Route::resource('register', RegisteredUserController::class);
+    Route::resource('login', AuthenticatedSessionController::class);
     Route::resource('jadwal', JadwalController::class);
     Route::resource('feedback', FeedbackController::class);
     Route::resource('payment', PaymentController::class);
+    Route::resource('paket', PaketController::class);
+    Route::resource('guru', GuruController::class);
     Route::resource('mapel', MapelController::class);
 
+    Route::get('/search', [SearchController::class, 'index'])->name('search');
+});
 
 require __DIR__.'/auth.php';
